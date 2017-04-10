e6dd29efee9e628f10082070d69dd4a89e156e0c
ganlvtech <ganlvtech@qq.com>
2017-04-10 23:19:51 +0800

    基本完成

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,14 +8,12 @@ class Index extends Controller
 {
     public function index()
     {
-        $departments = \app\common\model\Department::all();
-        $games = \app\common\model\Game::all();
+        $departments = \app\common\model\Department::order('medal_count', 'desc')->select();
+        $games14am = \app\common\model\Game::whereTime('begin_time', 'between', ['2017-04-14 00:00:00', '2017-04-14 11:59:59'])->select();
+        $games14pm = \app\common\model\Game::whereTime('begin_time', 'between', ['2017-04-14 12:00:00', '2017-04-14 23:59:59'])->select();
+        $games15am = \app\common\model\Game::whereTime('begin_time', 'between', ['2017-04-15 00:00:00', '2017-04-15 11:59:59'])->select();
+        $games15pm = \app\common\model\Game::whereTime('begin_time', 'between', ['2017-04-15 12:00:00', '2017-04-15 23:59:59'])->select();
         $posts = \app\common\model\Post::all();
-        array(
-            'departments' => $departments,
-            'games' => $games,
-            'posts' => $posts,
-        );
-        return $this->fetch('', compact('departments', 'games', 'posts'));
+        return $this->fetch('', compact('departments', 'games14am', 'games14pm', 'games15am', 'games15pm', 'posts'));
     }
 }
---
+++ public/static/index/src/main.js
@@ -0,0 +1,26 @@
+$(function () {
+    $('.time-table-day').on('click', function () {
+        $(this).find('.time-table-body').toggle();
+    });
+    $('#d14').on('click', function () {
+        $('.time-table-day').hide();
+        $('.m_14').show();
+    });
+    $('#d15').on('click', function () {
+        $('.time-table-day').hide();
+        $('.m_15').show();
+    });
+    $('#newsviewport-bg').on('click', function () {
+        $('#newsviewport-container').hide();
+    });
+    $('#newslist li').on('click', function () {
+        $.get($(this).attr('data-href'), function (data) {
+            $('#newsviewport').html(data);
+            $('#newsviewport-container').show();
+        });
+    });
+    $('#ranking_list').find('li:nth-child(n+5)').addClass('ranking-list-collapse').hide();
+    $('#more').on('click', function () {
+        $('.ranking-list-collapse').toggle();
+    });
+});

28261ea223526e78abee7fd2e08a312a0ec02beb
ganlvtech <ganlvtech@qq.com>
2017-04-14 01:49:09 +0800

    样式修改

--- public/static/index/src/main.js
+++ public/static/index/src/main.js
@@ -3,10 +3,22 @@ $(function () {
         $(this).find('.time-table-body').toggle();
     });
     $('#d14').on('click', function () {
+        $(this).css({
+            'background-color': 'rgba(0,0,0,0.7)'
+        })
+        $("#d15").css({
+            'background-color': 'rgba(0,0,0,0.3)'
+        })
         $('.time-table-day').hide();
         $('.m_14').show();
     });
     $('#d15').on('click', function () {
+        $(this).css({
+            'background-color': 'rgba(0,0,0,0.7)'
+        })
+        $("#d14").css({
+            'background-color': 'rgba(0,0,0,0.3)'
+        })
         $('.time-table-day').hide();
         $('.m_15').show();
     });
@@ -15,7 +27,7 @@ $(function () {
     });
     $('#newslist li').on('click', function () {
         $.get($(this).attr('data-href'), function (data) {
-            $('#newsviewport').html(data);
+            $('#newsviewport').append($('<div></div>').html(data));
             $('#newsviewport-container').show();
         });
     });
@@ -24,6 +36,9 @@ $(function () {
         $('.ranking-list-collapse').toggle();
     });
     $('#h5').on('click', function () {
-        location.href = 'http://h5.2017.eeyes.net';
+        location.href = 'http://h5-2017.eeyes.net';
     });
+    $("#close").click(function (event) {
+        $('#newsviewport-container').hide();
+    })
 });
